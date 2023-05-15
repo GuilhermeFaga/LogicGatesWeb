52121dc546c573f92de86068a861784595d8beaf
GuilhermeFaga <faga@retize.com.br>
2023-05-15 13:13:53 -0300

    feat: constantes

---
+++ src/constants/index.ts
@@ -0,0 +1,28 @@
+
+export const constants = {
+  colors: {
+    border: "000000",
+    on: "c3001c",
+    off: "525252",
+    board: "8e24c4",
+    andGate: "de6426",
+    notGate: "2856c5",
+  },
+  components: {
+    gate: {
+      padding: 16,
+      pinOffset: 24,
+      pinWidth: 32,
+      pinGap: 8
+    },
+    board: {
+      borderWidth: 5,
+      borderRadius: 8,
+      offset: 8
+    },
+    pin: {
+      borderWidth: 5,
+      width: 32
+    }
+  }
+}
