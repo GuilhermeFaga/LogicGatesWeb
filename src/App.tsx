8d7415ad1e2b54989f8a72d6f7545e70e3f2456f
GuilhermeFaga <faga@retize.com.br>
2023-05-15 13:17:25 -0300

    feat: interface básica e canvas do Pixi

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
+import { Stage } from '@pixi/react';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hud from './components/Hud';
+import AndGate from './gates/AndGate';
+import NotGate from './gates/NotGate';
+
+
 
 function App() {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const [height, setHeight] = React.useState(window.innerHeight);
+
+  window.addEventListener('resize', () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hud />
+      <Stage width={width} height={height} options={{ backgroundColor: 0x222222 }}>
+        <NotGate x={500} y={400} />
+        <AndGate x={500} y={250} />
+      </Stage>
+    </>
   );
 }
+
 
 export default App;
