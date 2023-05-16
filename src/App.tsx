63f47c5034ee3ad6ba8931e2873a08dc7294b762
GuilhermeFaga <faga@retize.com.br>
2023-05-15 21:31:10 -0300

    feat: adição do provider do redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { Sprite, Stage } from '@pixi/react';
 import { Texture } from 'pixi.js';
 import { useEffect, useState } from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
+import Connection from './components/Connection';
 import Hud from './components/Hud';
 import System from './components/System';
-import AndGate from './gates/AndGate';
-import NotGate from './gates/NotGate';
-import * as Logic from './logic';
+import { store } from './redux/store';
 
 
 export default function App() {
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
-
-  const [system, setSystem] = useState(new Logic.System(2));
 
   useEffect(() => {
     window.addEventListener('resize', () => {
@@ -25,13 +23,13 @@ export default function App() {
   return (
     <>
       <Hud />
-      <Stage width={width} height={height} options={{ backgroundColor: 0x222222 }}>
-        <System system={system} windowSize={[width, height]}>
-          <NotGate x={500} y={400} />
-          <AndGate x={500} y={250} />
-        </System>
-        {/* This sprite makes the interaction with the Graphics work. See https://github.com/pixijs/pixi-react/issues/402 */}
-        <Sprite texture={Texture.WHITE} height={0} width={0} />
+      <Stage width={width} height={height} options={{ backgroundColor: 0x222222, antialias: true }}>
+        <Provider store={store}>
+          <System windowSize={[width, height]} />
+          {/* This sprite makes the interaction with the Graphics work. See https://github.com/pixijs/pixi-react/issues/402 */}
+          <Sprite texture={Texture.WHITE} height={0} width={0} />
+          <Connection start={{ x: 50, y: 50 }} end={{ x: 250, y: 100 }} />
+        </Provider>
       </Stage>
     </>
   );
