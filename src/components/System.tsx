960acd8c603e934d0b90bcde19b834bee1f1afcd
GuilhermeFaga <faga@retize.com.br>
2023-05-15 21:30:41 -0300

    feat: configuração do sistema pelo redux e geração dos chips dinâmicamente

--- src/components/System.tsx
+++ src/components/System.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import { useEffect } from "react";
 import { constants } from "../constants";
-import * as Logic from "../logic";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import Chip from "./Chip";
 import InputPin from "./InputPin";
 import OutputPin from "./OutputPin";
+import { addChip } from "../redux/appReducer";
+import * as Logic from "../logic";
 
 
 interface Props {
-  system: Logic.System;
-  children: React.ReactNode;
   windowSize: [number, number];
 }
 
 export default function System(props: Props) {
-  const { system, children } = props;
+  const app = useAppSelector(state => state.app);
+  const dispatch = useAppDispatch();
+  const { system } = app;
+
+  useEffect(() => {
+    return () => {
+      dispatch(addChip(new Logic.NotGate()))
+    }
+  }, []);
 
   const [width, height] = props.windowSize;
 
@@ -23,14 +32,18 @@ export default function System(props: Props) {
   const firstPinY = (height / 2) + (-(pinGap / 2 * (len - 1)) - (pinRadius * (len - 1)));
 
   const inputs = system.systemInputs.map((input, i) => {
-    return <OutputPin key={i} x={54} y={firstPinY + (pinGap + pinRadius) * i} value={input.value} />;
+    return <OutputPin key={i} x={54} y={firstPinY + (pinGap + pinRadius) * i} outputPin={input} />;
   });
 
-  const output = <InputPin x={width - 54} y={height / 2} value={system.getValue()} />;
+  const output = <InputPin x={width - 54} y={height / 2} inputPin={system.systemOutput} />;
+
+  const chips = system.chips.map((chip, i) => {
+    return <Chip key={i} x={0} y={0} chip={chip} />;
+  })
 
   return (
     <>
-      {children as React.ReactElement}
+      {chips}
       {inputs}
       {output}
     </>
