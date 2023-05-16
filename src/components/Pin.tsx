40a368ac06b19387f2942cc4e0f5d5afe07804f7
GuilhermeFaga <faga@retize.com.br>
2023-05-15 21:27:23 -0300

    feat: eventos de hover e click nos Pins

--- src/components/Pin.tsx
+++ src/components/Pin.tsx
@@ -1,6 +1,6 @@
 import { Graphics } from '@pixi/react';
-import { Graphics as PIXI_Graphics } from 'pixi.js';
-import { useCallback } from 'react';
+import { FederatedEventHandler, FederatedPointerEvent, Graphics as PIXI_Graphics } from 'pixi.js';
+import { useCallback, useState } from 'react';
 import { constants } from '../constants';
 
 
@@ -8,6 +8,8 @@ interface Props {
   x: number;
   y: number;
   value: number,
+  onmousedown?: FederatedEventHandler<FederatedPointerEvent>;
+  onmouseup?: FederatedEventHandler<FederatedPointerEvent>;
 }
 
 const borderWidth = constants.components.pin.borderWidth;
@@ -15,20 +17,29 @@ const width = constants.components.pin.width;
 const radius = width / 2;
 
 export default function Pin(props: Props) {
+  const { x, y, value, onmousedown, onmouseup } = props;
 
-  const { x, y, value } = props;
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const baseColor = value ? constants.colors.on : constants.colors.off;
+  const mouseOverColor = value ? constants.colors.mouseOver.on : constants.colors.mouseOver.off;
+  const fillColor = isMouseOver ? mouseOverColor : baseColor;
 
   const draw = useCallback(
     (g: PIXI_Graphics) => {
       g.clear();
       g.beginFill(constants.colors.border);
       g.drawCircle(x, y, radius);
-      g.beginFill(value ? constants.colors.on : constants.colors.off);
+      g.beginFill(fillColor);
       g.drawCircle(x, y, radius - borderWidth);
       g.endFill();
     },
-    [x, y, value],
+    [x, y, fillColor],
   );
 
-  return <Graphics draw={draw} />;
+  return <Graphics draw={draw} eventMode='dynamic'
+    onmouseenter={() => setIsMouseOver(true)}
+    onmouseleave={() => setIsMouseOver(false)}
+    onmousedown={onmousedown || (() => { })}
+    onmouseup={onmouseup || (() => { })} />;
 }
