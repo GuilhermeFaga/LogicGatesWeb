b73521266fa0cb4c08a2d6ac19e95840ba7c7ca1
GuilhermeFaga <faga@retize.com.br>
2023-05-23 01:00:52 -0300

    refactor(EventsController): improved organization

--- src/components/EventsController.tsx
+++ src/components/EventsController.tsx
@@ -2,8 +2,9 @@ import { Container, useApp } from "@pixi/react";
 import { MutableRefObject, useCallback, useEffect, useState } from "react";
 import { clearSelectedChips, clearSelectedWires, handleSelectedChip, handleSelectedWire, setSelectedPin } from '../redux/appReducer';
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { Graphics } from "pixi.js";
+import { DisplayObject, Graphics } from "pixi.js";
 import * as Logic from "../logic";
+import { ThunkDispatch } from "@reduxjs/toolkit";
 
 interface Props {
   onMouseDownRef: MutableRefObject<(event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => void>;
@@ -56,57 +57,23 @@ export default function EventsController(props: Props) {
 
   function onMouseUp(event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
 
+    onClick(event);
+
+    setIsMouseDown(false);
+    setIsDragging(false);
+    dispatch(setSelectedPin(null));
+  }
+
+  function onClick(event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
+
     // if the mouse didn't move, then it's a click
     if (initialCursorPosition.x === event.clientX
       && initialCursorPosition.y === event.clientY) {
 
       let found = false;
 
-      // check if the mouse if over a wire
-      for (const wire of wires as Graphics[]) {
-
-        // convert the wire's points to screen coordinates
-        const points: { x: number, y: number }[] = [];
-
-        for (let i = 0; i < wire.geometry.points.length; i += 2) {
-          const x = wire.geometry.points[i];
-          const y = wire.geometry.points[i + 1];
-          points.push({ x, y });
-        }
-
-        points.reverse();
-        for (let i = 0; i < points.length; i++) {
-          const point = points[i];
-          const nextPoint = points[i + 1];
-
-          if (!nextPoint) break;
-
-          const threshold = 10;
-          if (point.x - threshold <= event.clientX && event.clientX <= nextPoint.x + threshold
-            && point.y - threshold <= event.clientY && event.clientY <= point.y + threshold) {
-            const inputPosition = points[points.length - 1];
-            const outputPosition = points[0]
-            const logicWire = Logic.Wire.findWireFromPoints(logicWires, inputPosition, outputPosition);
-            if (!logicWire) break;
-            found = true;
-            dispatch(handleSelectedWire(logicWire));
-            dispatch(clearSelectedChips());
-            break;
-          }
-        }
-      }
-
-      // check if the mouse is over a chip
-      for (const chip of chips) {
-        if (!chip.rect) continue;
-        if (chip.rect.contains(event.clientX, event.clientY)) {
-          // if so, select it
-          console.log(chip);
-          found = true;
-          dispatch(handleSelectedChip(chip));
-          dispatch(clearSelectedWires());
-        }
-      }
+      found = wiresSelectionHandler(event, dispatch, { wires, logicWires });
+      found = chipsSelectionHandler(event, dispatch, { chips }) || found;
 
       if (!found) {
         // if none of the chips or wires were selected, clear the selection
@@ -115,9 +82,6 @@ export default function EventsController(props: Props) {
       }
     }
 
-    setIsMouseDown(false);
-    setIsDragging(false);
-    dispatch(setSelectedPin(null));
   }
 
   function onMouseMove(event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
@@ -130,4 +94,75 @@ export default function EventsController(props: Props) {
     <Container eventMode='dynamic'>
     </Container>
   );
+}
+
+function wiresSelectionHandler(
+  event: React.MouseEvent<HTMLCanvasElement, MouseEvent>,
+  dispatch: ThunkDispatch<any, any, any>,
+  params: { wires: DisplayObject[], logicWires: Logic.Wire[] }
+) {
+
+  const { wires, logicWires } = params;
+
+  let found = false;
+
+  // check if the mouse if over a wire
+  for (const wire of wires as Graphics[]) {
+
+    // convert the wire's points to screen coordinates
+    const points: { x: number, y: number }[] = [];
+
+    for (let i = 0; i < wire.geometry.points.length; i += 2) {
+      const x = wire.geometry.points[i];
+      const y = wire.geometry.points[i + 1];
+      points.push({ x, y });
+    }
+
+    points.reverse();
+    for (let i = 0; i < points.length; i++) {
+      const point = points[i];
+      const nextPoint = points[i + 1];
+
+      if (!nextPoint) break;
+
+      const threshold = 10;
+      if (point.x - threshold <= event.clientX && event.clientX <= nextPoint.x + threshold
+        && point.y - threshold <= event.clientY && event.clientY <= point.y + threshold) {
+        const inputPosition = points[points.length - 1];
+        const outputPosition = points[0]
+        const logicWire = Logic.Wire.findWireFromPoints(logicWires, inputPosition, outputPosition);
+        if (!logicWire) break;
+        found = true;
+        dispatch(handleSelectedWire(logicWire));
+        dispatch(clearSelectedChips());
+        break;
+      }
+    }
+  }
+
+  return found;
+}
+
+function chipsSelectionHandler(
+  event: React.MouseEvent<HTMLCanvasElement, MouseEvent>,
+  dispatch: ThunkDispatch<any, any, any>,
+  params: { chips: Logic.Chip[] }
+) {
+
+  const { chips } = params;
+
+  let found = false;
+
+  // check if the mouse is over a chip
+  for (const chip of chips) {
+    if (!chip.rect) continue;
+    if (chip.rect.contains(event.clientX, event.clientY)) {
+      // if so, select it
+      found = true;
+      dispatch(handleSelectedChip(chip));
+      dispatch(clearSelectedWires());
+    }
+  }
+
+  return found;
 }
