250a0e4f09f35ef5bacd2144b5fb0ee2f27937e3
GuilhermeFaga <faga@retize.com.br>
2023-05-27 17:32:35 -0300

    feat: dragging chip scale animation

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,11 +1,12 @@
 import { Container, useApp, useTick } from '@pixi/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { config } from 'src/config';
 import * as Logic from 'src/logic';
 import { useAppSelector } from 'src/redux/hooks';
 import Board from './Board';
 import InputPin from './InputPin';
 import OutputPin from './OutputPin';
+import { useAnimation } from 'src/util/animation';
 
 
 interface Props {
@@ -34,6 +35,8 @@ export default function Chip(props: Props) {
   const [isMouseDown, setIsMouseDown] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
 
+  const [scale, setScale] = useAnimation(1, 10);
+
   const [cursorXOffset, setCursorXOffset] = useState(0);
   const [cursorYOffset, setCursorYOffset] = useState(0);
 
@@ -41,11 +44,20 @@ export default function Chip(props: Props) {
 
   const app = useApp();
   useTick((delta) => {
-    if (isDragging) {
-      setX(app.renderer.events.pointer.global.x - cursorXOffset);
-      setY(app.renderer.events.pointer.global.y - cursorYOffset);
+    if (isDragging || scale !== 1) {
+      setX(app.renderer.events.pointer.global.x - cursorXOffset * scale);
+      setY(app.renderer.events.pointer.global.y - cursorYOffset * scale);
     }
   });
+
+  useEffect(() => {
+    if (isDragging) {
+      setScale(1.2);
+    } else {
+      setScale(1);
+    }
+    // eslint-disable-next-line
+  }, [isDragging]);
 
   const padding = config.components.gate.padding;
   const pinOffset = config.components.gate.pinOffset;
@@ -61,7 +73,7 @@ export default function Chip(props: Props) {
   const output = <OutputPin x={width} y={height / 2 + 8} outputPin={chip.output} />;
 
   return (
-    <Container position={[x, y]} name={chip.id}>
+    <Container position={[x, y]} name={chip.id} scale={scale}>
       <Board height={height} width={width} color={color} selected={selected}
         onmousedown={(event) => {
           setCursorXOffset(event.global.x - x);
